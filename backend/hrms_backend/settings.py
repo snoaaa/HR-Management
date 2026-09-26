5d814c680a2269f546d9ec7e45cb58af6b1ebf00
unknown <ab.noumsi@gmail.com>
2026-09-26 02:35:51 +0100

    register organisation app

--- backend/hrms_backend/settings.py
+++ backend/hrms_backend/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "users",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
+    "organisation",
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
